dd4fdc4b8811a0de918e9ba8d2c798215cca24fb
Leandro <leandro.accioly@mesquita.rj.gov.br>
2017-04-26 15:35:31 -0300

    alteração no  menu lateral

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -8,9 +8,7 @@
         
         <!-- menu profile quick info -->
         <div class="profile">
-            <div class="profile_pic">
-                {{-- <img src="{{ Gravatar::src(Auth::user()->email) }}" alt="Avatar of {{ Auth::user()->name }}" class="img-circle profile_img"> --}}
-            </div>
+            
             <div class="profile_info">
                 <span>Bem Vindo!</span>
                 <h2>{{ Auth::user()->name }}</h2>
@@ -19,57 +17,24 @@
         <!-- /menu profile quick info -->
         
         <br />
-        
+
+         
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3 class="home">Home</h3>
+                <h3 class="home fa fa-home">Home</h3>
                 <ul class="nav side-menu">
-                    <li><a><i class=""></i>Usuário<span class="fa fa-chevron-down"></span></a>
+                    <li><a><i class=" fa fa-user"></i>Usuário<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="#">Cadastro de Usuário</a></li>
                             <li><a href="#">Lista de Usuário</a></li>
                             <li><a href="#">Alteração de Usuário</a></li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="javascript:void(0)">
-                            <i class="fa fa-laptop"></i>
-                            One link
-                            <span class="label label-success pull-right">Flag</span>
-                        </a>
-                    </li>
+                    
                 </ul>
             </div>
-            <div class="menu_section">
-                <h3>Group 2</h3>
-                <ul class="nav side-menu">
-                    <li>
-                        <a><i class="fa fa-sitemap"></i> Multilevel Menu <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li>
-                                <a href="#">Level One</a>
-                                <li>
-                                    <a>Level One<span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li class="sub_menu">
-                                            <a href="#">Level Two</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Level Two</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Level Two</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            <li>
-                                <a href="#">Level One</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+          
         
         </div>
         <!-- /sidebar menu -->
