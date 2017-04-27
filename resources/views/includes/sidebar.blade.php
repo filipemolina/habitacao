ea066d36e655511530f4dd7fa6d66446651dbe48
Leandro <leandro.accioly@mesquita.rj.gov.br>
2017-04-27 10:14:09 -0300

    alteração no  menu lateral e top barra

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -13,22 +13,37 @@
                 <span>Bem Vindo!</span>
                 <h2>{{ Auth::user()->name }}</h2>
             </div>
+            <div class="clearfix"></div>
         </div>
         <!-- /menu profile quick info -->
         
         <br />
 
-         
+
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3 class="home fa fa-home">Home</h3>
+             
                 <ul class="nav side-menu">
+                    <li>
+                        <a href="{{ url("/") }}">
+                            <i class="fa fa-home"></i>
+                            Pagina Inicial
+                        </a>
+                    </li>
+                    <li><a><i class=" fa fa-male"></i>Pesoas<span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="{{ url('pessoas/create') }}">Cadastro de Pessoas</a></li>
+                            <li><a href="{{ url('pessoas') }}">Lista de Pessoas</a></li>
+                           
+                        </ul>
+                    </li>
+                    
                     <li><a><i class=" fa fa-user"></i>Usuário<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a href="#">Cadastro de Usuário</a></li>
-                            <li><a href="#">Lista de Usuário</a></li>
-                            <li><a href="#">Alteração de Usuário</a></li>
+                            <li><a href="{{ url('users/create') }}">Cadastro de Usuário</a></li>
+                            <li><a href="{{ url('users') }}">Lista de Usuário</a></li>
+                           
                         </ul>
                     </li>
                     
@@ -39,21 +54,6 @@
         </div>
         <!-- /sidebar menu -->
         
-        <!-- /menu footer buttons -->
-        <div class="sidebar-footer hidden-small colormenu">
-            <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-            </a>
-            <a data-toggle="tooltip" data-placement="top" title="Logout" href="{{ url('/logout') }}">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-            </a>
-        </div>
-        <!-- /menu footer buttons -->
+       
     </div>
 </div>
