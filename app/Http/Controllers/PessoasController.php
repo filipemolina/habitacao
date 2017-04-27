8e1096233a38c347a56c7c9bf0a854eb2e525c34
Filipe Molina <filipemolina@live.com>
2017-04-27 10:34:09 -0300

    Ajustes

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PessoasController extends Controller
 {
-    /**
+     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
