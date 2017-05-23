98812243bbd2ea5a3ac5b8b2228fdaa82559a783
Leandro <leandro.accioly@hotmail.com>
2017-05-23 13:52:11 -0300

    atualizações

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view colormenu">
         <div class="navbar nav_title colormenu" style="border: 0;">
-            <a href="{{ url('/') }}" class="site_title"><i class="fa fa-home"></i> <span>Habitação</span></a>
+           <a href="{{ url('/') }}" class="site_title"><i class="fa fa-home"></i> <span>Habitação</span></a>
         </div>
         
         <div class="clearfix"></div>
