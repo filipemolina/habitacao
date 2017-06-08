93f47cd8be923f2bcdbe8dc1d42f2c636326433d
Leandro <leandro.accioly@hotmail.com>
2017-06-08 12:03:51 -0300

    correção do campo parentesco

--- resources/views/pessoas/create.blade.php
+++ resources/views/pessoas/create.blade.php
@@ -313,7 +313,28 @@
           <div class="col-md-7">
             <input value="{{ old('coparticipante.nome') }}" id="coparticipante[nome]" name="coparticipante[nome]" type="text" placeholder="Informe o nome" class="form-control input-md nome" > <span class="obrigatorio">*</span>
           </div>
+
+          <label class="col-md-1 control-label" for="dependentes.0.parentesco">Parentesco</label>
+          <div class="col-md-2">  <span class="obrigatorio">*</span>
+            <select name="dependentes[0][parentesco]" id="dependentes[0][parentesco]" type="text" placeholder="Necessidades Especiais" class="form-control input-md" >
+              <option value="" disabled @if(!old('dependentes.0.parentesco')) selected @endif>Selecione...</option>
+            <option value="1" @if(old('dependentes.0.parentesco') == "1") selected="selected" @endif>Avô(ó)</option>
+            <option value="2" @if(old('dependentes.0.parentesco') == "2") selected="selected" @endif>Bisavô(ó)</option>
+            <option value="3" @if(old('dependentes.0.parentesco') == "3") selected="selected" @endif>Bisneto(a)</option>
+            <option value="4" @if(old('dependentes.0.parentesco') == "4") selected="selected" @endif>Companheiro(a)</option>
+            <option value="5" @if(old('dependentes.0.parentesco') == "5") selected="selected" @endif>Cônjuge</option>
+            <option value="6" @if(old('dependentes.0.parentesco') == "6") selected="selected" @endif>Enteado(a)</option>
+            <option value="7" @if(old('dependentes.0.parentesco') == "7") selected="selected" @endif>Ex-esposa</option>
+            <option value="8" @if(old('dependentes.0.parentesco') == "8") selected="selected" @endif>Filho(a)</option>
+            <option value="9" @if(old('dependentes.0.parentesco') == "9") selected="selected" @endif>Irmão(ã)</option>
+            <option value="10" @if(old('dependentes.0.parentesco') == "10") selected="selected" @endif>Neto(a)</option>
+            <option value="11" @if(old('dependentes.0.parentesco') == "11") selected="selected" @endif>Pais</option>
+            <option value="12" @if(old('dependentes.0.parentesco') == "12") selected="selected" @endif>Outras</option>
+            </select>
+          </div>
         </div>
+
+
         
         {{-- CPF, NIS/PIS, Bolsa Família e Carteira de trabalho --}}
         <div class="form-group">
@@ -551,8 +572,8 @@
         {{-- Nome --}}
         <div class="form-group">
           <label class="col-md-1 control-label" for="dependentes[0][nome]">Nome</label>
-          <div class="col-md-7">
-            <input value="{{ old('dependentes.0.nome') }}" name="dependentes[0][nome]" id="dependentes[0][nome]" type="text" placeholder="Informe o nome" class="form-control input-md" > <span class="obrigatorio">*</span>
+          <div class="col-md-7"> <span class="obrigatorio">*</span>
+            <input value="{{ old('dependentes.0.nome') }}" name="dependentes[0][nome]" id="dependentes[0][nome]" type="text" placeholder="Informe o nome" class="form-control input-md" > 
           </div>
 
           <label class="col-md-1 control-label" for="dependentes.0.parentesco">Parentesco</label>
