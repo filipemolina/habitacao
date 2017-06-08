341ab3a7a33fbb287eeb6c70f527da83aa6aac2a
Leandro <leandro.accioly@hotmail.com>
2017-06-08 11:54:15 -0300

    correção do campo obrigatorio

--- resources/views/pessoas/create.blade.php
+++ resources/views/pessoas/create.blade.php
@@ -45,14 +45,14 @@
 
     {{-- Mostrar mensagem de sucesso --}}
 
-    {{-- @if(session('sucesso')) --}}
+    @if(session('sucesso'))
 
         <div class="alert alert-dourado alert-dismissible" style="margin-top: 70px;" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <strong>Parabéns!</strong> {{-- session('sucesso') --}}
+          <strong>Parabéns!</strong> {!! session('sucesso') !!}
         </div>
 
-    {{-- @endif --}}
+    @endif
 
     <h2> Cadastro de Pessoa </h2>
 
