83a10f2baa4e18f539b5d09131d8db8522dc4bed
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2018-08-10 10:07:56 -0300

    bug

--- app/Http/Controllers/ParticipantesController.php
+++ app/Http/Controllers/ParticipantesController.php
@@ -561,9 +561,9 @@ class ParticipantesController extends Controller
 
         $participante = Participante::with('coparticipante', 'telefones', 'endereco', 'dependentes', 'coparticipante.telefones', 'coparticipante.endereco')->where("id", $id)->first();
 
-        $qtd_coparticipante = (is_array($pessoa->coparticipante) ? count($pessoa->coparticipante) : 0);
+        $qtd_coparticipante = (is_array($participante->coparticipante) ? count($participante->coparticipante) : 0);
 
-        $pdf = PDF::loadView('pessoas.relatorios.comprovante', compact('participante','$qtd_coparticipante'));
+        $pdf = PDF::loadView('pessoas.relatorios.comprovante', compact('participante','qtd_coparticipante'));
 
         return $pdf->stream();
 
